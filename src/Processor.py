bcba0fd313802e8e845c6a11fafd6b7cf0f8ec55
CengizhanYurdakul <cengizhanyurdakul97@gmail.com>
2024-02-02 14:10:39 +0300

    Docstrings added

--- src/Processor.py
+++ src/Processor.py
@@ -26,6 +26,9 @@ class Processor:
             self.initMLSDDetector()
         
     def initVariables(self):
+        """
+        Defines the variables to be used in the class and makes format changes.
+        """
         self.stableDiffusionModel = None
         self.controlNetModel = None
         self.depthEstimator = None
@@ -38,35 +41,66 @@ class Processor:
         print("[Processor] - Variables initialized!")
             
     def initControlNetModel(self):
+        """
+        Initialises the ControlNet class. Calls the ControlNet model from Factory 
+        according to the method [canny - segmentation - depth - hed - mlsd] to be used.
+        """
         self.controlnetModel = ControlNetFactory(self.args).callModel()
         print("[Processor] - Controlnet initialized!")
         
     def initStableDiffusionModel(self):
+        """
+        Initialises the Stable Diffusion class. Calls the appropriate Stable Diffusion 
+        model according to the ControlNet method to be used.
+        """
         self.stableDiffusionModel = StableDiffusionFactory(self.args).callModel(self.controlnetModel)
         print("[Processor] - Stable Diffusion model initialized!")
         
     def initDepthEstimator(self):
+        """
+        Initialises the depth estimation structure required for the preprocess.
+        """
         from transformers import pipeline
         self.depthEstimator = pipeline("depth-estimation")
         print("[Processor] - Depth estimator initialized!")
     
     def initSegmentationNetwork(self):
+        """
+        Initialises the semantic segmentation structure required for the preprocess.
+        """
         from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
         self.imageProcessor = AutoImageProcessor.from_pretrained("openmmlab/upernet-convnext-small")
         self.imageSegmentator = UperNetForSemanticSegmentation.from_pretrained("openmmlab/upernet-convnext-small")
         print("[Processor] - Segmentation model initialized!")
         
     def initHEDDetector(self):
+        """
+        Initialises the HED detector structure required for the preprocess.
+        """
         from controlnet_aux import HEDdetector
         self.hedDetector = HEDdetector.from_pretrained('lllyasviel/ControlNet')
         print("[Processor] - HED initialized!")
         
     def initMLSDDetector(self):
+        """
+        Initialises the MLSD detector structure required for the preprocess.
+        """
         from controlnet_aux import MLSDdetector
         self.mlsdDetector = MLSDdetector.from_pretrained('lllyasviel/ControlNet')
         print("[Processor] - MLSD initialized!")
         
-    def processImage(self):
+    def processImage(self) -> Image:
+        """
+        Processes the input image according to the method to control the output with ControlNet.
+        When a new method is added, its preprocess is implemented here.
+
+        Raises:
+            Exception: If you try to preprocess a ControlNet method that has not been implemented 
+            before, you will end up here.
+
+        Returns:
+            Image: Output of the image to be used for ControlNet in PIL.Image format.
+        """
         if self.args.controlnetMethod == "canny":
             cannyImage = processCanny(self.inputImage)
             print("[Processor] - Image processed with canny!")
@@ -90,7 +124,19 @@ class Processor:
         else:
             raise Exception("Sorry, process for %s method not implemented yet!" % self.args.controlnetMethod)
         
-    def main(self, inputImage:np.array, prompt:str) -> np.array:
+    def main(self, inputImage:np.array, prompt:str) -> Image:
+        """
+        Processes the input image received from the user. It takes the style that comes as
+        input from the prompt dictionary. It takes inference from Stable Diffusion model
+        and output image is obtained.
+
+        Args:
+            inputImage (np.array): RGB format image received by the user
+            prompt (str): It is the style chosen by the user
+
+        Returns:
+            Image: It is the image processed by the Stable Diffusion pipeline. It is returned in Image.PIL format.
+        """
         
         self.inputImage = inputImage
         
@@ -103,4 +149,4 @@ class Processor:
             image=processedImage
             ).images[0]
         
-        return outputImage, processedImage
+        return outputImage
